a8fca1aa53120615296d1d7703bfc81301e9f389
SergioEanX <sergio.ferlito@enea.it>
2024-02-25 11:11:06 +0100

    added colors to fern logger

--- src/utils.rs
+++ src/utils.rs
@@ -1,30 +1,32 @@
-
-use std::time::SystemTime;
 use humantime;
+use log::LevelFilter;
+use std::time::SystemTime;
+
+use fern::colors::{Color, ColoredLevelConfig};
 pub fn add(x: i32, y: i32) -> i32 {
     x + y
 }
 
-
-
-
-
-
-
 pub fn setup_logger() -> Result<(), fern::InitError> {
+    let colors = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        .info(Color::Green)
+        .debug(Color::Magenta)
+        .trace(Color::Cyan);
+
     fern::Dispatch::new()
-        .format(|out, message, record| {
+        .format(move |out, message, record| {
             out.finish(format_args!(
-                "[{} {} {}] {}",
+                "{} [{} {}] {}",
+                colors.color(record.level()),
                 humantime::format_rfc3339_seconds(SystemTime::now()),
-                record.level(),
                 record.target(),
                 message
             ))
         })
-        .level(log::LevelFilter::Debug)
+        .level(LevelFilter::Debug)
         .chain(std::io::stdout())
-        .chain(fern::log_file("output.log")?)
         .apply()?;
     Ok(())
 }
